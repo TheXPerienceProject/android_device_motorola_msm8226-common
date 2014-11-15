b241f54fbe0d17049793e2d3acd886362f703743
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-11-14 20:05:42 -0600

    Enable dex-preoptimization to speed up first boot sequence
    Change-Id: Ic8295333e12b69a473654a0dc061d08d6b3e8120
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -191,3 +191,14 @@ EXTENDED_FONT_FOOTPRINT := true
 
 #Doug Lea's Mallog a Memory allocator
 MALLOC_IMPL := dlmalloc
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+ ifeq ($(TARGET_BUILD_VARIANT),user)
+ ifeq ($(WITH_DEXPREOPT),)
+ WITH_DEXPREOPT := true
+ WITH_DEXPREOPT_BOOT_IMG_ONLY := false
+ endif
+ endif
+endif
+WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
