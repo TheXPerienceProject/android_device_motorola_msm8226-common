8b025d6f5067b047485e154dd5890241a4eb799d
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-11-16 12:55:46 -0600

    stagefright: use media_codecs_google*.xml files
    Change-Id: Ie41e6dbecd87c8344e7f792e8bbf039f3ac58a37
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- msm8226.mk
+++ msm8226.mk
@@ -43,6 +43,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+# Media
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+    
 # System Properties
 -include $(LOCAL_PATH)/system_prop.mk
 
