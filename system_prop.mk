47d485527c06a427deba8bbe9f95031c5b7451e8
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-01-03 18:50:27 -0600

    msm8226: Fix VoIP issues
    Change-Id: I0498657050e49e5be30fadfe614416889b595d1e
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -36,7 +36,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.ssr=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    use.dedicated.device.for.voip=true \
+    use.dedicated.device.for.voip=false \
     use.voice.path.for.pcm.voip=true
 
 # Bluetooth
