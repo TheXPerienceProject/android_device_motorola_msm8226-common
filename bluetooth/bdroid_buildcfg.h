8c1edb67ad7ae799174272531fd2ea88413c2eb6
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-01-07 06:41:09 -0600

    msm8226-common: bluetooth: Update for lollipop Bring in-line with LA.BF.1.1.2_rb1.10.
    Change-Id: I97a021692d362d11479d2b40a1d5da70a6755208
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,15 +19,10 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Moto G"
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BLUETOOTH_QCOM_SW TRUE
-#define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
-#define BTC_INCLUDED TRUE
-// Enables Interleave scan
-#define BTA_HOST_INTERLEAVE_SEARCH  TRUE
+#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
-#define MAX_L2CAP_CHANNELS    14
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_L2CAP_CHANNELS    16
 // skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  TRUE
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
 #endif
