ae4892393a6fc0d7540399c25c147778da6d07dc
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-01-07 05:59:43 -0600

    msm8226-common: Don't use separate VOIP
    Change-Id: I615d923ad8f481ab42f67e521c9ad31922a1762e
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -36,7 +36,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.ssr=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    use.dedicated.device.for.voip=false \
     use.voice.path.for.pcm.voip=true
 
 # Bluetooth
