c9cbe4250eac249c9b65ae5c2e571fa3961dba54
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-01-07 19:58:45 -0600

    revert: "msm8226: No more SELinux permissive "
    Change-Id: I7bd460d935669c5df283e44b23ccbbf23d8177a1
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,7 +45,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8226
 TARGET_NO_BOOTLOADER 	     := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE      := console=ttyHSL0,115200,n8 earlyprintk androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags androidboot.write_protect=0
+BOARD_KERNEL_CMDLINE      := console=ttyHSL0,115200,n8 earlyprintk androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags androidboot.write_protect=0 androidboot.selinux=permissive
 BOARD_KERNEL_BASE         := 0x00000000
 BOARD_KERNEL_PAGESIZE     := 2048
 BOARD_KERNEL_SEPARATED_DT := true
@@ -186,7 +186,7 @@ WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 USE_MINIKIN := true
 
 ### Webkit and browser do not loose time on it remove if not maked a first build before
-PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
+#PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
@@ -213,7 +213,7 @@ BOARD_SEPOLICY_UNION += \
     batt_health.te \
     device.te \
     file_contexts \
-   file.te \
+    file.te \
     init.te \
     init_shell.te \
     keystore.te \
