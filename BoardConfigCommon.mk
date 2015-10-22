577ff70e2dff286afac31fe7ff2e99ecc0f7f669
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-10-22 05:51:16 -0500

    Revert "msm8226-common: Disable kernel modules"
    This reverts commit 1c5c4362d4d66757d5191bfd76416c9d683f8b46.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,6 +54,13 @@ BOARD_KERNEL_PAGESIZE     := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS      := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 
+WLAN_MODULES:
+	mkdir -p $(KERNEL_MODULES_OUT)/pronto
+	mv $(KERNEL_MODULES_OUT)/wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
+	ln -sf /system/lib/modules/pronto/pronto_wlan.ko $(TARGET_OUT)/lib/modules/wlan.ko
+
+TARGET_KERNEL_MODULES += WLAN_MODULES
+
 # Audio
 AUDIO_FEATURE_ENABLED_FM                    := true
 AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS  := true
@@ -132,16 +139,16 @@ BOARD_VOLD_MAX_PARTITIONS 	    	:= 40
 TARGET_USE_CUSTOM_LUN_FILE_PATH     := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
 # Wifi
-BOARD_HAS_QCOM_WLAN                 := true
-BOARD_HAS_QCOM_WLAN_SDK             := true
-BOARD_HOSTAPD_DRIVER                := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB           := lib_driver_cmd_qcwcn
-BOARD_WLAN_DEVICE                   := qcwcn
-BOARD_WPA_SUPPLICANT_DRIVER         := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_qcwcn
-WIFI_DRIVER_FW_PATH_AP              := "ap"
-WIFI_DRIVER_FW_PATH_STA             := "sta"
-WPA_SUPPLICANT_VERSION              := VER_0_8_X
+BOARD_HAS_QCOM_WLAN 		 	 := true
+BOARD_WLAN_DEVICE   		 	 := qcwcn
+BOARD_HAS_QCOM_WLAN_SDK 		 := true
+BOARD_HOSTAPD_DRIVER   		 	 := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB 	 	 := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER 	 := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_MODULE_PATH  	 	 := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME 	 	 := "wlan"
+WPA_SUPPLICANT_VERSION 	  	 	 := VER_0_8_X
 
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
@@ -196,4 +203,7 @@ webcrhom:
 	ln -sf /system/lib/libwebviewchromium.so $(TARGET_OUT)/app/Webview/lib/arm/libwebviewchromium.so
 
 TARGET_WEBVIEW += webcrhom
+
+
+##config for neew recovery based on philz
 
