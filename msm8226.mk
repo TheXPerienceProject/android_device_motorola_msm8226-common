36208c0c7a814612defc49d5981ce1d4ea652bd8
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-11-03 00:37:47 -0600

    msm8226: add again ethernet support :u
    Change-Id: I4b5f2f73b73d6e73504e740b2be7b2033c6e12c2
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- msm8226.mk
+++ msm8226.mk
@@ -28,6 +28,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
@@ -140,7 +141,9 @@ PRODUCT_PACKAGES += \
     libOmxCore \
     libOmxVdec \
     libOmxVenc \
+    libOmxVidcCommon \
     libstagefrighthw \
+    libextmedia_jni \
     qcmediaplayer
 
 PRODUCT_BOOT_JARS += \
