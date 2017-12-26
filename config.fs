127575f062c58d3126a268bd872f968b5ca59316
klozz <klozz707@gmail.com>
2017-12-26 05:56:28 -0500

    msm8226-common: Re-enable qcom aids
    Signed-off-by: klozz <klozz707@gmail.com>

--- config.fs
+++ config.fs
@@ -1,11 +1,11 @@
-#[AID_QCOM_DIAG]
-#value:2950
+[AID_QCOM_DIAG]
+value:2950
 
-#//[AID_RFS]
-#//value:2951
+[AID_RFS]
+value:2951
 
-#[AID_RFS_SHARED]
-#value:2952
+[AID_RFS_SHARED]
+value:2952
 
 [AID_MOT_ACCY]
 value:5000
