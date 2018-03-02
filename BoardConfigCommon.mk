dd0601d6948bde08ca10bd9db9c6243b747fc9e7
klozz <klozz707@gmail.com>
2018-03-02 07:13:44 -0500

    msm8226-common: Set the correct shims
    Change-Id: I0b422b58fad8b4cdd65405e463d821ff14b9a270
    Signed-off-by: klozz <klozz707@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,7 +145,13 @@ WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Shims
-TARGET_LD_SHIM_LIBS := /system/vendor/bin/thermal-engine|libshims_thermal.so:/system/vendor/bin/mpdecision|libshims_atomic.so:/system/lib/hw/camera.vendor.msm8226.so|libshims_camera.so:/system/lib/libmot_sensorlistener.so|libshims_sensorlistener.so
+TARGET_LD_SHIM_LIBS := \
+    /system/vendor/bin/thermal-engine|libshims_thermal.so \
+    /system/vendor/bin/mpdecision|libshims_atomic.so \
+    /system/lib/hw/camera.vendor.msm8226.so|libshims_camera.so \
+    /system/lib/libmot_sensorlistener.so|libshims_sensorlistener.so \
+    /system/vendor/lib/libqc-opt.so|libshim_qcopt.so \
+    /system/vendor/bin/mm-qcamera-daemon|libshim_canvas.so
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
