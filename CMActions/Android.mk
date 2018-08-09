76462b78f55446a6c9feed67e28bbc20d548828c
klozz <shaclone707@gmail.com>
2018-08-09 19:47:58 -0400

    msm8226-common: set LOCAL_SDK_VERSION
    Signed-off-by: klozz <shaclone707@gmail.com>

--- CMActions/Android.mk
+++ CMActions/Android.mk
@@ -8,6 +8,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := CMActions
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_SDK_VERSION := current
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v14-preference \
