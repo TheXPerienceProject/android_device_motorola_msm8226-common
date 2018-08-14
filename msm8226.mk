4f63e295b44ce62041e3b441b426dda5135775f1
Kshitij Gupta <kshitijgm@gmail.com>
2018-08-14 16:16:50 +0000

    msm8226-common: Import modified android.hardware.bluetooth@1.0-impl
    - Import and include modified HIDL
    - Is same as AOSP but has
      https://android.googlesource.com/platform/hardware/interfaces/+/8d0d6f99dfbd121140549adc43f360686467e875
      reverted
    
    Signed-off-by: klozz <shaclone707@gmail.com>

--- msm8226.mk
+++ msm8226.mk
@@ -87,6 +87,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-impl.msm8226 \
     libbt-vendor
 
 # Camera
