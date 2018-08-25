5d635aea02ef7f24b2833207d43ed366de2605bd
klozz <shaclone707@gmail.com>
2018-08-24 21:31:02 -0400

    msm8226-common:: CameraWrapper: add some libs
    Signed-off-by: klozz <shaclone707@gmail.com>

--- camera/Android.mk
+++ camera/Android.mk
@@ -7,7 +7,9 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libgui libutils libbase
+    libhardware liblog libcamera_client libgui libutils libbase libhidltransport libsensor \
+		android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
