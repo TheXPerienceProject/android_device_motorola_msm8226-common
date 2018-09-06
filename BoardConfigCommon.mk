cf61c745a920e2ef4f82639fd058668ae08ac3dd
klozz <shaclone707@gmail.com>
2018-09-06 07:16:14 -0400

    msm8226-common: Enable Sepolicy
    Signed-off-by: klozz <shaclone707@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,10 +170,9 @@ endif
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
 # SELinux
-#-include device/qcom/sepolicy/sepolicy.mk
-#-include device/qcom/sepolicy/legacy-sepolicy.mk
-
-#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+-include device/qcom/sepolicy/sepolicy.mk
+-include device/qcom/sepolicy/legacy-sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
