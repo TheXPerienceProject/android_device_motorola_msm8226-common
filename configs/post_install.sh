3b6a73cff29077f1d8445d8db8d32ca9f3065cb4
klozz <shaclone707@gmail.com>
2018-12-13 19:34:33 -0500

    msm8226-common:Don't remove CalendarProvider
    Signed-off-by: klozz <shaclone707@gmail.com>

--- configs/post_install.sh
+++ configs/post_install.sh
@@ -5,4 +5,3 @@ rm -rf /system/app/CommandCenter3
 rm -rf /system/app/XPerienceWallpapers
 rm -rf /system/app/XPeriaWeather
 rm -rf /system/priv-app/XPeriaHome
-rm -rf /system/priv-app/CalendarProvider
