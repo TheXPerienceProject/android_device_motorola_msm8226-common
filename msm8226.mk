21d5dff99da03683cf49689b74b80d6f62a05592
klozz <shaclone707@gmail.com>
2019-01-13 03:22:33 -0500

    msm8226-common: drop libbt-vendor
    Signed-off-by: klozz <shaclone707@gmail.com>

--- msm8226.mk
+++ msm8226.mk
@@ -99,8 +99,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    libbt-vendor
+    android.hardware.bluetooth@1.0-impl
 
 # Camera
 PRODUCT_PACKAGES += \
