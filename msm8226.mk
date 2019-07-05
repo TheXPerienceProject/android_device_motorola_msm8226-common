a4c678cee986e1a0c7d54ed9e112e994d498ced6
klozz <klozz@vivaldi.net>
2019-07-05 09:44:49 +0000

    sm8226-common: Binderize bluetooth
    Signed-off-by: klozz <klozz@vivaldi.net>

--- msm8226.mk
+++ msm8226.mk
@@ -102,7 +102,9 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor
 
 # Camera
 PRODUCT_PACKAGES += \
