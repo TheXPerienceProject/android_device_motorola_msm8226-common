68c1858ca80b9c2a820a7819d543b1aa2d60cb55
klozz <klozz@vivaldi.net>
2019-07-06 22:36:35 +0000

    msm8226-common: overlay cleanups
    Signed-off-by: klozz <klozz@vivaldi.net>

--- msm8226.mk
+++ msm8226.mk
@@ -26,8 +26,7 @@ include device/qcom/common/common.mk
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-#    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
