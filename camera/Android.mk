62e0367a4e4981d9a1f3ae2a619e0be03ae88e64
Steven Moreland <smoreland@google.com>
2019-10-01 11:01:16 -0700

    msm8226-common: Remove libhwbinder/libhidltransport deps
    Since these were combined into libhidlbase.
    
    Bug: 135686713
    Test: build only (libhwbinder/libhidltransport are empty)
    Change-Id: I640a3f0e0839214523627bf0b59fd5ae9dd2c61c
    Signed-off-by: klozz <klozz@vivaldi.net>

--- camera/Android.mk
+++ camera/Android.mk
@@ -7,8 +7,8 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libgui libutils libbase libhidltransport libsensor \
-		android.hidl.token@1.0-utils \
+    libhardware liblog libcamera_client libgui libutils libbase libhidlbase libsensor \
+    android.hidl.token@1.0-utils \
     android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
