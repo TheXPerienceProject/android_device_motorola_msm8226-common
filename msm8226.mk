dbb42bd998fbe4354c6c0d8122a1d7ca9bdaa988
RenanQueiroz <queirozrrq@gmail.com>
2019-11-02 03:34:01 -0400

    msm8226-common: Regenerate manifest

--- msm8226.mk
+++ msm8226.mk
@@ -171,6 +171,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.mapper@2.1 \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     android.hardware.configstore@1.1-service \
     copybit.msm8226 \
     gralloc.msm8226 \
